24f09a2fe8ae54f5bda395b63c604324cae3fe34
Bhumit Singh <chiragsingh360@gmail.com>
2025-04-06 21:52:36 +0530

    New Files Added

---
+++ NumLan/NumLan/NumLan.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+
+using namespace std;
+
+vector<double> gaussianElimination(vector<vector<double>> A, vector<double> b) {
+	int n = A.size();
+
+	// Forward elimination
+	for (int i = 0; i < n; i++) {
+		//Pivot
+		int maxRow = i;
+		for (int k = i + 1; k < n; k++) {
+			if 
+		}
+	}
+}
